9cbf10a9aa7dc1880ef917b23b5de1e5df0baa7c
Sam Byers <sam.byers@ucdconnect.ie>
2023-03-20 18:04:16 +0000

    new commit

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <string.h> /* for strlen */
 #include <unistd.h> /*for read and write and pipe*/
 #include <sys/types.h>
+#include <limits.h>
 
 #define _GNU_SOURCE
 #define promptchar '#'
@@ -10,12 +11,22 @@
 int main()
 {
     FILE *termpoint;
-
+    char cwd[100 * sizeof(char)];
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+    {
+        printf("Current working dir: %s\n", cwd);
+    }
+    else
+    {
+        perror("getcwd() error");
+        exit(1);
+    }
     int end = 0;
     int pid;
     char *dataEnt = NULL;
     char *token, *savepointer;
     char delim[2] = {' ', '\0'};
+    char * synthargv[100];
     termpoint = stdin;
 
     if (termpoint == NULL)
@@ -34,19 +45,20 @@ int main()
             if (dataEnt[i] == '\n')
                 (dataEnt[i] = '\0');
         }
-        for (int j = 1;; j++, dataEnt = NULL)
+        for (int j = 0;; j++, dataEnt = NULL)
         {
             token = __strtok_r(dataEnt, delim, &savepointer);
             if (token == NULL)
             {
                 printf("\n");
-                printf("%c",promptchar);
+                printf("%c", promptchar);
                 break;
             }
-
             printf("\n%d:%s", j, token);
+            strcpy(synthargv[j],token);
+            printf("\n %s",synthargv[j]);
         }
+        execvp(cwd, synthargv);
     }
-
     return 0;
 }
