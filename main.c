0416ff9c89b92da8892cab3ca52d02831d6eb1da
Sam Byers <sam.byers@ucdconnect.ie>
2023-03-20 23:06:39 +0000

    Much further along

--- main.c
+++ main.c
@@ -3,15 +3,39 @@
 #include <string.h> /* for strlen */
 #include <unistd.h> /*for read and write and pipe*/
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <limits.h>
+#include <time.h>
+#include <signal.h>
 
 #define _GNU_SOURCE
 #define promptchar '#'
 
+void updatetime(time_t timedata, struct tm *info) // update the time and print the prompt
+{
+    char buffer[80];
+    time(&timedata);
+    info = localtime(&timedata);
+    strftime(buffer, 80, "%d/%m %H:%M", info);
+    printf("[%s]%c ", buffer, promptchar);
+}
+
+void signalhandle(int signum) // handle the signal, flush the input and output, return carrage
+{
+    fflush(stdout);
+    fflush(stdin);
+    printf("\r");
+}
+
 int main()
 {
-    FILE *termpoint;
-    char cwd[100 * sizeof(char)];
+    signal(SIGINT, signalhandle);
+    FILE *termpoint;    // initalise the terminal pointer
+    time_t timedata;    // initalise the timedata variable
+    struct tm *info;    // and the time info struct that will hold the timedata
+    char cwd[100];      // current working directory array, limited to 100 chars
+    int customstat = 0; // custom status int, if the command being executed is 'custom', do not execvp
+    int status;         // the status of the execvp command
     char *command[50];
     if (getcwd(cwd, sizeof(cwd)) != NULL)
     {
@@ -25,8 +49,8 @@ int main()
     int end = 0;
     int pid;
     char *dataEnt = NULL;
-    char *token, *savepointer;
-    char delim[2] = {' ', '\0'};
+    char *token, *savepointer;   // the token pointer and the pointer that contains the rest of the input string
+    char delim[2] = {' ', '\0'}; // the deliminator that is used when tokenising the input string
     termpoint = stdin;
 
     if (termpoint == NULL)
@@ -36,11 +60,11 @@ int main()
 
     size_t len = 0;
     ssize_t nread;
-    printf("%c", promptchar);
-    while (nread = getline(&dataEnt, &len, termpoint) != -1)
+    updatetime(timedata, info);
+    while (nread = getline(&dataEnt, &len, termpoint) != -1) // take user input into the dataEnt array
     {
-        printf("%c", promptchar);
-        for (int i = 0; i < strlen(dataEnt); i++)
+        customstat = 0;                           // customstat defaults to 0
+        for (int i = 0; i < strlen(dataEnt); i++) // change the newline to a null char
         {
             if (dataEnt[i] == '\n')
                 (dataEnt[i] = '\0');
@@ -48,29 +72,58 @@ int main()
         for (int j = 0;; j++, dataEnt = NULL)
         {
             token = __strtok_r(dataEnt, delim, &savepointer);
-            if (token == NULL)
+
+            if (token == NULL) // when we hit the last token in the dataEnt pointer print a newline
             {
                 printf("\n");
-                printf("%c", promptchar);
                 break;
             }
-            printf("\n%d:%s", j, token);
-            command[j] = token;
-            command[j + 1] = NULL;
+            command[j] = token;    // put the token in place
+            command[j + 1] = NULL; // command must be terminated with a null pointer
+        }
+        if (command[0] != NULL)
+        {
+            if (strcmp(command[0], "exit") == 0) // if the command is exit, exit
+                exit(0);
+            if (strcmp(command[0], "cd") == 0) // if the command is CD
+            {
+                customstat = 1;
+                if (command[1] != NULL)
+                {
+                    chdir(command[1]);
+                }
+                else
+                {
+                    printf("\nNo argument given for directory!\nChanging directory to home\n");
+                    chdir(getenv("HOME"));
+                }
+            }
         }
         if (fork() == 0)
         {
-            int status = execvp(command[0], command);
-            if (status == -1)
+            // If fork returns a zero value, child process
+
+            if (customstat == 0)
+                status = execvp(command[0], command); // exe the command, this will kill the child
+            else
             {
-                printf("Terminated Incorrectly\n");
-                return 1;
+                status = 1; // status is 1 if the command is custom, assume succuess of command
+                return 0;   // kill the child
+            }
+            if (status == -1) // if command failed
+            {
+                printf("Invalid command: %s\n", command[0]); // print invalid command
+                return 1;                                    // kill the child
             }
         }
         else
         {
-            // Old Parent process. The C program will come here
-            // if fork() returns non-zero value.
+            // If fork returns a non zero value
+            // reset the command array to NULL so that pressing enter does not run the last command again
+            for (int i = 0; command[i] != NULL; i++)
+                command[i] = NULL;
+            wait(NULL);                 // voodoo witchcraft, This blocks the parent process until a child process has completed
+            updatetime(timedata, info); // print the next prompt for user input
         }
     }
 
