d10ffc98672e80469779a8dbbbfa1bf9c2870fbc
Sam Byers <sam.byers@ucdconnect.ie>
2023-03-23 21:41:30 +0000

    Final commit with the tar.gz package

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 This is a simple shell that uses execvp to execute commands, with cd functionality.
 Use on a unix system in the terminal
 
+The shell command './configure && make && make install'
+should configure, build, and install this package from the .tar.gz file
+
+It can then be run by the shell command:
+./assign2_21315391
+
 To write to a file, just do
 $ (your command) > filename
 where filename is the name of your desired destination file.
@@ -10,6 +16,6 @@ where filename is the name of your desired destination file.
 
 *any command before or after CTRL + C will be ingnored*
 
-- Sam Byers - 2023
+- © - Sam Byers - 2023 - 
 - https://github.com/sam-byers/SHELL
 
--- main.c
+++ main.c
@@ -1,3 +1,28 @@
+/*--------------------------------------------
+This is a simple shell that uses execvp to execute commands, with cd functionality.
+Use on a unix system in the terminal
+
+The shell command './configure && make && make install'
+should configure, build, and install this package from the .tar.gz file
+
+It can then be run by the shell command:
+./assign2_21315391
+
+To write to a file, just do
+$ (your command) > filename
+where filename is the name of your desired destination file.
+
+Doing CTRL + C will not exit the shell program, but you must press return to return to a new line
+
+any command before or after CTRL + C will be ingnored
+
+
+- © - Sam Byers - 2023 - 
+- https://github.com/sam-byers/SHELL
+
+            VERSION 1.0 - [23 - 3 - 23]
+-----------------------------------------------*/
+
 #include "functions.h" //include the functions
 
 int main()
